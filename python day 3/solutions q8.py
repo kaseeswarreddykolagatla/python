a71f6640238ed4512039480e28dadd49e62e63b3
kaseeswarreddykolagatla <123532622+kaseeswarreddykolagatla@users.noreply.github.com>
2023-02-10 14:04:26 +0530

    Add files via upload

---
+++ python day 3/solutions q8.py
@@ -0,0 +1,12 @@
+class solutions:
+    def groupAnagrams(self, strs):
+        result = {}
+        for i in strs:
+        x = "".join(sorted(i))
+        if x in result:
+            result[x].append(i)
+        else:
+            result[x] = [i]
+  return list(result.values())
+ob1 = solutions()
+print(ob1.groupangrams(["eat", "tea", "tan", "ate", "nat", "bat"]))
